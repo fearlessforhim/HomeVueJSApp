f77477640a71e0245a9f1a790942490ce9977593
Jonathan Cameron <jonathancameron6@protonmail.com>
2020-08-29 07:43:50 -0700

    Start work on single post pages

---
+++ src/SingleBlogPost.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main-blog-wrap">
+        <j-row>
+            <j-cell
+                    :c="9"
+            >
+                <div>Here's the post!</div>
+            </j-cell>
+            <j-cell
+                    :c="3"
+            >
+                <SidePanel/>
+            </j-cell>
+        </j-row>
+    </div>
+</template>
+
+<script>
+    import SidePanel from "./components/SidePanel";
+    import PostContainer from "./components/PostContainer";
+    export default {
+        name: "SingleBlogPost",
+        components: {PostContainer, SidePanel}
+    }
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import About from "../About";
 import MainBlog from "../MainBlog";
+import SingleBlogPost from "../SingleBlogPost";
 
 Vue.use(VueRouter)
 
@@ -18,7 +19,12 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: About
-  }
+  },
+    {
+      path: 'blog',
+      name: 'Blog Post',
+      component: SingleBlogPost
+    }
 ]
 
 const router = new VueRouter({
