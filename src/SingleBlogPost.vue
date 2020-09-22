ff65789c58575838c2c1334b7b41a06d6c7560b4
Jonathan Cameron <jonathancameron6@protonmail.com>
2020-09-22 11:23:13 -0700

    Add styling for single blog post

--- src/SingleBlogPost.vue
+++ src/SingleBlogPost.vue
@@ -40,7 +40,7 @@
         },
         computed: {
             formattedContent() {
-                if(this.post.content) {
+                if (this.post.content) {
                     return this.post.content.replace(/(?:\r\n|\r|\n)/g, '<br>');
                 }
                 return '';
@@ -56,4 +56,39 @@
 </script>
 
 <style scoped lang="scss">
+    .single-post-item {
+        margin: 0 90px 0 0;
+
+        .post-title {
+            font-size: 24px;
+        }
+
+        .post-content {
+            border-top: 1px solid $panel-border;
+            padding: 10px;
+
+            > p {
+                line-height: 26px;
+                text-align: justify;
+                font-size: 18px
+            }
+        }
+
+        .date-created {
+            color: darken($panel-border, 10%);
+            float: right;
+            padding-top: 10px;
+        }
+    }
+
+    @media only screen and (max-width: 600px) {
+        .post-item {
+            margin: 0;
+            padding: 10px;
+
+            .post-content {
+                font-size: 20px;
+            }
+        }
+    }
 </style>
