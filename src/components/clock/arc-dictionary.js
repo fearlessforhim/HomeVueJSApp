6cbf482c13a9589ddf5f29e7e3e2cd9334735b10
Jonathan Cameron <jonathancameron6@protonmail.com>
2023-09-07 20:38:11 -0700

    Update time arcs

--- src/components/clock/arc-dictionary.js
+++ src/components/clock/arc-dictionary.js
@@ -1,60 +1,47 @@
 import {svgArcBuilder} from './svg-arc-builder.js';
-import { faSun, faTshirt, faBowlFood, faTooth, faCarSide, faDumbbell, faBookBible } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faTshirt, faBowlFood, faTooth, faCarSide, faDumbbell, faBookBible, faBrush, faShoePrints } from '@fortawesome/free-solid-svg-icons';
 
 export function arcDictionary() {
-    const hb = 6;
+    const hb = 19;
     return [
-        {//Bible study bar
-            showTime: new Date().setHours(hb, 0, 0, 0),
-            stopTime: new Date().setHours(hb, 45, 0, 0),
-            arcConfig: {startMinute: 0, lengthInMinutes: 15, color: '#00D0E8', rounded: false},
-            textLabel: {
-                position: {
-                    top: '50px',
-                    right: '-40px'
-                },
-                text: 'Bible Study',
-                icon: faBookBible,
-                iconKey: 'bookBible'
-            }
-        },{//exercise bar
-            showTime: new Date().setHours(hb, 0, 0, 0),
-            stopTime: new Date().setHours(hb, 45, 0, 0),
-            arcConfig: {startMinute: 15, lengthInMinutes: 35, color: 'yellow', rounded: false},
-            textLabel: {
-                position: {
-                    bottom: '0px',
-                    right: '-40px'
-                },
-                text: 'I work out!',
-                icon: faDumbbell,
-                iconKey: 'dumbbell'
-            }
-        },
         {//get dressed bar
             showTime: new Date().setHours(hb, 0, 0, 0),
             stopTime: new Date().setHours(hb+1, 0, 0, 0),
-            arcConfig: {startMinute: 40, lengthInMinutes: 20, color: 'green', rounded: false},
+            arcConfig: {startMinute: 40, lengthInMinutes: 15, color: 'green', rounded: false},
             textLabel: {
                 position: {
-                    top: '50px',
-                    left: '0px'
+                    top: '350px',
+                    left: '-200px'
                 },
-                text: 'Get Dressed!',
+                text: 'Get Dressed / Make Bed',
                 icon: faTshirt,
                 iconKey: 'shirt'
             }
         },
-        {//eat and shoes bar
+        {
+            showTime: new Date().setHours(hb, 0, 0, 0),
+            stopTime: new Date().setHours(hb+1, 0, 0, 0),
+            arcConfig: {startMinute: 55, lengthInMinutes: 5, color: 'blue', rounded: false},
+            textLabel: {
+                position: {
+                    top: '20px',
+                    left: '150px'
+                },
+                text: 'Brush Hair/Appliance',
+                icon: faBrush,
+                iconKey: 'bed-brush'
+            }
+        },
+        {
             showTime: new Date().setHours(hb, 45, 0, 0),
             stopTime: new Date().setHours(hb+2, 0, 0, 0),
-            arcConfig: {startMinute: 0, lengthInMinutes: 30, color: 'red', rounded: false},
+            arcConfig: {startMinute: 0, lengthInMinutes: 25, color: 'red', rounded: false},
             textLabel: {
                 position: {
                     top: '50px',
                     right: '0px'
                 },
-                text: 'Eat / Shoes On',
+                text: 'Eat',
                 icon: faBowlFood,
                 iconKey: 'bowlFoot'
             }
@@ -62,15 +49,29 @@ export function arcDictionary() {
         {//brush teeth bar
             showTime: new Date().setHours(hb+1, 0, 0, 0),
             stopTime: new Date().setHours(hb+2, 0, 0, 0),
-            arcConfig: {startMinute: 30, lengthInMinutes: 10, color: '#E600FF', rounded: false},
+            arcConfig: {startMinute: 25, lengthInMinutes: 10, color: '#E600FF', rounded: false},
             textLabel: {
                 position: {
                     bottom: '0px',
-                    left: '0px'
+                    left: '275px'
                 },
                 text: 'Brush Teeth!',
                 icon: faTooth,
                 iconKey: 'shirt'
+            }
+        },
+        {
+            showTime: new Date().setHours(hb+1, 0, 0, 0),
+            stopTime: new Date().setHours(hb+2, 0, 0, 0),
+            arcConfig: {startMinute: 35, lengthInMinutes: 5, color: 'brown', rounded: false},
+            textLabel: {
+                position: {
+                    bottom: '50px',
+                    left: '0px'
+                },
+                text: 'Shoes',
+                icon: faShoePrints,
+                iconKey: 'shoe'
             }
         },
         {//to the car bar
@@ -82,7 +83,7 @@ export function arcDictionary() {
                     top: '350px',
                     left: '-150px'
                 },
-                text: 'To the car!',
+                text: 'Shoes / To the car!',
                 icon: faCarSide,
                 iconKey: 'car'
             }
